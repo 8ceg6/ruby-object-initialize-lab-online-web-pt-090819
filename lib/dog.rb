1feb1a323d2326e18df2582248435d4600922b7e
cedric e goddard <8ceg6@comcast.net>
2019-10-09 20:50:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog 
-    attr_accessor :name , :breed 
-    def initalize(name)
+    attr_accessor :name  
+    def initalize(name, )
       @name= name
     end 
     def initalize(breed = "Mutt")
